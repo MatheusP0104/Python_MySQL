4263c6848faa7d17c870d87323a3825f2c4d7bd8
MatheusP0104 <105940986+MatheusP0104@users.noreply.github.com>
2022-05-20 16:47:16 -0300

    Banco_Senac

---
+++ Banco_Senac.sql
@@ -0,0 +1,5 @@
+create database Banco_Senac;
+use Banco_Senac;
+
+create table produtos (id_produto int not null primary key, nome_produto varchar(45), valor_produto float);
+select * from produtos;
